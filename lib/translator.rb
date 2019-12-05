9965ef57e9310d1b3a547ab5b1cb454b0d770959
Nick Corona <coronaisme@gmail.com>
2019-12-05 22:32:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,9 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
   sorry_message = "Sorry, that emoticon was not found"
+  
+  result = emoticon_hash["get_emoticon"][emoticon]
   if result == nil 
     result = sorry_message
 end
